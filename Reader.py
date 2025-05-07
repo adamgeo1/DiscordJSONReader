a8f35011ecb68a26e2ff6986d195f299ff0d30ef
adamgeo1 <adamgeo66@gmail.com>
2025-05-07 13:48:54 -0400

    working version, need to fix replies and to mark when there was an attachment. also need to change from .txt output to .md output

---
+++ Reader.py
@@ -0,0 +1,71 @@
+import json
+import pathlib
+
+INPUT_PATH = pathlib.Path(__file__).parent.absolute() / 'Input' # Input JSON Discord logs folder
+OUTPUT_PATH = pathlib.Path(__file__).parent.absolute() / 'Output' # Output formatted Discord messages folder
+
+class Message:
+    ids_and_names = {}
+
+    def __init__(self, line_num, message):
+        self.line_num = line_num
+        self.id = message.get('author', {}).get('id')
+        self.author = message.get('author', {}).get('global_name') or message.get('author', {}).get('username')
+        self.content = message.get('content', "")
+        self.timestamp = message.get('timestamp', "")[:10] + " " + message.get('timestamp', "")[11:19]
+        self.mentioned_id = None
+        if message.get('mentions'):
+            self.mentioned_id = message['mentions'][0].get('id')
+        self.referenced_message_id = None
+        if message.get('referenced_message'):
+            self.referenced_message_id = message['referenced_message'].get('id')
+            self.referenced_message = message['referenced_message']
+        Message.ids_and_names[self.id] = self.author
+
+    def replace_id_with_name(self):
+        words = self.content.split()
+        new_words = []
+        for word in words:
+            if word.startswith("<@") and word.endswith(">"):
+                user_id = word[2:-1]
+                name = Message.ids_and_names.get(user_id, f"@{user_id}")
+                new_words.append(f"@{name}")
+            else:
+                new_words.append(word)
+        return " ".join(new_words)
+
+    def __str__(self):
+        message_line = f"{self.line_num} |  {self.timestamp} | {self.author}: \""
+        reply_info = ""
+        if self.referenced_message_id:
+            reply = getattr(self, 'referenced_message', None)
+            if reply and reply.get('author'):
+                reply_name = reply['author'].get('global_name') or reply['author'].get('username')
+                reply_info = f"Replying to @{reply_name} - "
+        return message_line + reply_info + self.replace_id_with_name()
+
+    #TODO add something to show there was an attachment and change from using .txt to .md for better looking stuff
+
+def main():
+    for file in INPUT_PATH.iterdir():
+        if file.is_file():
+
+            messages = []
+
+            with open(file, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                for channel, message_list in data.items():
+                    line_num = 1
+                    messages.append(f"{channel}:\n")
+                    for i in range(len(data[channel]) - 1, -1, -1):
+                        messages.append(Message(line_num, data[channel][i]))
+                        line_num += 1
+                    messages.append("")
+
+            with open(OUTPUT_PATH / f"{file.name}Output.txt", 'w', encoding='utf-8') as f:
+                for message in messages:
+                    f.write(str(message) + "\n")
+
+if __name__ == '__main__':
+    main()
+
