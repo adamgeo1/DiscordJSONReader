04a437277c6e50bd25f90164a27c3fc55801f4aa
adamgeo1 <adamgeo66@gmail.com>
2025-05-08 14:14:56 -0400

    switched from txt to md, added line number reference for replies

--- Reader.py
+++ Reader.py
@@ -5,11 +5,13 @@ INPUT_PATH = pathlib.Path(__file__).parent.absolute() / 'Input' # Input JSON Dis
 OUTPUT_PATH = pathlib.Path(__file__).parent.absolute() / 'Output' # Output formatted Discord messages folder
 
 class Message:
-    ids_and_names = {}
+    author_ids_and_names = {}
+    message_ids_and_line_numbers = {}
 
     def __init__(self, line_num, message):
         self.line_num = line_num
-        self.id = message.get('author', {}).get('id')
+        self.message_id = message.get('id', 0)
+        self.author_id = message.get('author', {}).get('id')
         self.author = message.get('author', {}).get('global_name') or message.get('author', {}).get('username')
         self.content = message.get('content', "")
         self.timestamp = message.get('timestamp', "")[:10] + " " + message.get('timestamp', "")[11:19]
@@ -20,7 +22,8 @@ class Message:
         if message.get('referenced_message'):
             self.referenced_message_id = message['referenced_message'].get('id')
             self.referenced_message = message['referenced_message']
-        Message.ids_and_names[self.id] = self.author
+        Message.author_ids_and_names[self.author_id] = self.author
+        Message.message_ids_and_line_numbers[self.message_id] = self.line_num
 
     def replace_id_with_name(self):
         words = self.content.split()
@@ -28,23 +31,23 @@ class Message:
         for word in words:
             if word.startswith("<@") and word.endswith(">"):
                 user_id = word[2:-1]
-                name = Message.ids_and_names.get(user_id, f"@{user_id}")
+                name = Message.author_ids_and_names.get(user_id, f"@{user_id}")
                 new_words.append(f"@{name}")
             else:
                 new_words.append(word)
         return " ".join(new_words)
 
     def __str__(self):
-        message_line = f"{self.line_num} |  {self.timestamp} | {self.author}: \""
+        message_line = f"{self.line_num}. `{self.timestamp}`  {self.author}: \""
         reply_info = ""
         if self.referenced_message_id:
             reply = getattr(self, 'referenced_message', None)
             if reply and reply.get('author'):
                 reply_name = reply['author'].get('global_name') or reply['author'].get('username')
-                reply_info = f"Replying to @{reply_name} - "
+                reply_line_num = Message.message_ids_and_line_numbers.get(str(reply.get('id')), 0)
+                reply_info = f"Replying to @{reply_name} from line `{reply_line_num}` - "
         return message_line + reply_info + self.replace_id_with_name()
 
-    #TODO add something to show there was an attachment and change from using .txt to .md for better looking stuff
 
 def main():
     for file in INPUT_PATH.iterdir():
@@ -56,13 +59,14 @@ def main():
                 data = json.load(f)
                 for channel, message_list in data.items():
                     line_num = 1
-                    messages.append(f"{channel}:\n")
+                    messages.append(f"## {channel}:\n")
                     for i in range(len(data[channel]) - 1, -1, -1):
                         messages.append(Message(line_num, data[channel][i]))
                         line_num += 1
-                    messages.append("")
+                    messages.append("") # line spacing between channel names
 
-            with open(OUTPUT_PATH / f"{file.name}Output.txt", 'w', encoding='utf-8') as f:
+            with open(OUTPUT_PATH / f"{file.name}Output.md", 'w', encoding='utf-8') as f:
+                f.write(f"# {file.name}\n")
                 for message in messages:
                     f.write(str(message) + "\n")
 
