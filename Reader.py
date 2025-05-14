9ea48fa597936bf398a0120020894ae3c419b16f
adamgeo1 <adamgeo66@gmail.com>
2025-05-14 12:21:44 -0400

    refactored to use arguments, provide error messages, increase readability, and fixed formatting issue

--- Reader.py
+++ Reader.py
@@ -2,9 +2,16 @@ import json
 import pathlib
 from datetime import datetime, time
 import math
+import argparse
 
-INPUT_PATH = pathlib.Path(__file__).parent.absolute() / 'Input' # Input JSON Discord logs folder
-OUTPUT_PATH = pathlib.Path(__file__).parent.absolute() / 'Output' # Output formatted Discord messages folder
+parser = argparse.ArgumentParser(description="Discord JSON Log Parser")
+parser.add_argument("--input", type=str, default=pathlib.Path(__file__).parent.absolute() / 'Input', help="Specify input directory (must be exact path)")
+parser.add_argument("--output", type=str, default=pathlib.Path(__file__).parent.absolute() / 'Output', help="Specify output directory (must be exact path)")
+parser.add_argument("--no-stats", action='store_true', help="Disable stats calculation")
+args = parser.parse_args()
+
+INPUT_PATH = args.input # Input JSON Discord logs folder
+OUTPUT_PATH = args.output # Output formatted Discord messages folder
 
 def format_timestamp(string):
     return string[:10] + " " + string[11:19]
@@ -56,7 +63,7 @@ class Message:
                 new_words.append(f"@{name}")
             else:
                 new_words.append(word)
-        return " ".join(new_words) + "\"" # returns modified content message (the end " was lost somewhere along the way)
+        return " ".join(new_words) # returns modified content message
 
     def get_time_difference(self, message): # function to find how long it took for someone to reply to another person
         try:
@@ -67,16 +74,17 @@ class Message:
             return None
 
     def __str__(self):
-        message_line = f"{self.line_num}. `{self.timestamp}`  {self.author}: \"" # prefix for every message
+        message_line = f"{self.line_num}. `{self.timestamp}`  {self.author}:" # prefix for every message
         reply_info = ""
         if self.referenced_message_id:
             if self.referenced_message and self.referenced_message.get('author'): # just makes sure they exist
                 reply_name = self.referenced_message['author'].get('global_name') or self.referenced_message['author'].get('username')
                 reply_line_num = Message.message_ids_and_line_numbers.get(str(self.referenced_message.get('id')), 0)
-                reply_info = f"Replying to @{reply_name} from line `{reply_line_num}` - " # adds if they are replying to someone to the output string
+                reply_info = f" Replying to @{reply_name} from line `{reply_line_num}` - " # adds if they are replying to someone to the output string
         if self.attachments:
             message_line += f"(Attachments: {self.attachments}) "
-        return message_line + reply_info + self.replace_id_with_name() # returns final output message
+        content = f" \"{self.replace_id_with_name()}\""
+        return message_line + reply_info + content # returns final output message
 
 
 def calculate_time_stats(messages):
@@ -98,54 +106,71 @@ def calculate_time_stats(messages):
     return author_stats
 
 
+def stats_output(file, author_time_stats):
+    with open(OUTPUT_PATH / f"{file.name[:-5]}Statistics.txt", 'w', encoding='utf-8') as f:
+        f.write(f"{file.name[:-5]} Statistics\n")  # adds the input file name to the top
+        f.write(f"\nTotal number of words per person:\n")
+        for author in Message.author_names_and_total_words:
+            f.write(f"\t- {author}: {Message.author_names_and_total_words[author]}\n")
+        f.write(f"\nNumber of messages per person:\n")
+        for author in Message.author_names_and_number_of_messages:
+            Message.author_names_and_number_of_messages[author] > 1 and f.write(
+                f"\t- {author}: {Message.author_names_and_number_of_messages[author]}\n")
+        f.write(f"\nNumber of messages sent between 8 AM and 5 PM per person:\n")
+        for author in Message.author_names_and_number_of_messages_sent_between_eight_and_five:
+            f.write(
+                f"\t- {author}: {Message.author_names_and_number_of_messages_sent_between_eight_and_five[author]}\n")
+        f.write(f"\nTime response statistics per person:\n")  # line breaks
+        for author, (avg, standard_deviation, num_responses) in author_time_stats.items():
+            f.write(f"\t- {author}:\n")
+            f.write(f"\t\t- Total responses: {num_responses}\n")
+            f.write(f"\t\t- Average response time: {avg:.2f} seconds\n")
+            f.write(f"\t\t- Standard deviation: {standard_deviation:.2f} seconds\n")
+
 def main():
 
-    for file in INPUT_PATH.iterdir(): # iterates over every file in the input path
-        if file.is_file():
+    if not INPUT_PATH.exists():
+        print("⚠️ No Input directory found, please either specify using \"--input\" or make sure it exists in the CWD")
+        exit(1)
 
-            messages = []
+    elif not any(INPUT_PATH.iterdir()):
+        print("⚠️ No files found in Input directory")
+        exit(1)
 
-            with open(file, 'r', encoding='utf-8') as f:
-                data = json.load(f) # loads json file
-                for channel, message_list in data.items():
-                    line_num = 1
-                    messages.append(f"### **{channel}**\n") # displays channel name
-                    for i in range(len(data[channel]) - 1, -1, -1): # the json is saved in reverse chronological order, so we iterate backwards
-                        messages.append(Message(line_num, data[channel][i], channel))
-                        line_num += 1
-                    messages.append("") # line spacing between channel names
+    else:
 
-            with open(OUTPUT_PATH / f"{file.name}Output.md", 'w', encoding='utf-8') as f:
-                f.write(f"## **Output**\n")
-                for message in messages:
-                    f.write(str(message) + "\n")
+        for file in INPUT_PATH.iterdir(): # iterates over every file in the input path
 
-            author_time_stats = calculate_time_stats(messages)
+            if file.is_file() and file.suffix == ".json":
 
-            with open(OUTPUT_PATH / f"{file.name}Statistics.txt", 'w', encoding='utf-8') as f:
-                f.write(f"{file.name}\n")  # adds the input file name to the top
-                f.write(f"\nTotal number of words per person:\n")
-                for author in Message.author_names_and_total_words:
-                    f.write(f"\t- {author}: {Message.author_names_and_total_words[author]}\n")
-                f.write(f"\nNumber of messages per person:\n")
-                for author in Message.author_names_and_number_of_messages:
-                    Message.author_names_and_number_of_messages[author] > 1 and f.write(f"\t- {author}: {Message.author_names_and_number_of_messages[author]}\n")
-                f.write(f"\nNumber of messages sent between 8 AM and 5 PM per person:\n")
-                for author in Message.author_names_and_number_of_messages_sent_between_eight_and_five:
-                    f.write(f"\t- {author}: {Message.author_names_and_number_of_messages_sent_between_eight_and_five[author]}\n")
-                f.write(f"\nTime response statistics per person:\n") # line breaks
-                for author, (avg, standard_deviation, num_responses) in author_time_stats.items():
-                    f.write(f"\t- {author}:\n")
-                    f.write(f"\t\t- Total responses: {num_responses}\n")
-                    f.write(f"\t\t- Average response time: {avg:.2f} seconds\n")
-                    f.write(f"\t\t- Standard deviation: {standard_deviation:.2f} seconds\n")
+                messages = []
 
-            # resetting all dicts for the next file
-            Message.author_ids_and_names.clear()
-            Message.author_names_and_total_words.clear()
-            Message.author_names_and_number_of_messages.clear()
-            Message.author_names_and_number_of_messages_sent_between_eight_and_five.clear()
-            Message.message_ids_and_line_numbers.clear()
+                with open(file, 'r', encoding='utf-8') as f:
+                    data = json.load(f) # loads json file
+                    for channel, message_list in data.items():
+                        line_num = 1
+                        messages.append(f"### **{channel}**\n") # displays channel name
+                        for i in range(len(data[channel]) - 1, -1, -1): # the json is saved in reverse chronological order, so we iterate backwards
+                            messages.append(Message(line_num, data[channel][i], channel))
+                            line_num += 1
+                        messages.append("") # line spacing between channel names
+
+                with open(OUTPUT_PATH / f"{file.name[:-5]}Output.md", 'w', encoding='utf-8') as f:
+                    f.write(f"## **{file.name[:-5]} Output**\n")
+                    for message in messages:
+                        f.write(str(message) + "\n")
+
+                author_time_stats = calculate_time_stats(messages)
+
+                if not args.no_stats: # only if user didn't pass "--no-stats" argument
+                    stats_output(file, author_time_stats)
+
+                # resetting all dicts for the next file
+                Message.author_ids_and_names.clear()
+                Message.author_names_and_total_words.clear()
+                Message.author_names_and_number_of_messages.clear()
+                Message.author_names_and_number_of_messages_sent_between_eight_and_five.clear()
+                Message.message_ids_and_line_numbers.clear()
 
 
 if __name__ == '__main__':
